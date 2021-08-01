d7c76a0c6dfb15c5081b3dea853bb235ba77a871
Roberto Sanchez <rsanchez92@live.com>
2021-08-01 12:24:53 -0700

    Added another route

--- app.py
+++ app.py
@@ -4,11 +4,16 @@ import pickle
 import zipfile
 
 app = Flask(__name__)
+# Loading model
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
 	message = "HELLO WORLD"
 	return message
+
+@app.route('/predict', methods=['GET','POST'])
+def predict():
+	return "Not Hotdog"
 
 if __name__ == '__main__':
 	app.run(debug=True)
