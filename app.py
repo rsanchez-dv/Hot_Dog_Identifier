f3e585ffdbd60c92869ae783f15ae934a7dc8e1f
Roberto Sanchez <rsanchez92@live.com>
2021-09-13 06:24:04 -0700

    Added resources to TF for better performance

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, render_template
-import os
-import io
 from PIL import Image
-import zipfile
 from tensorflow import keras
 import numpy as np
 from keras.preprocessing import image
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = '-1'
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
 
 app = Flask(__name__)
 # Loading model
@@ -26,8 +26,8 @@ def predict():
     rawImage = request.files["image"].read()
     # Converts bytes into usable format
     print(rawImage)
-    imageData = Image.frombytes('RGB', (180, 180), rawImage)
-    print(imageData)
+    imageData = Image.frombytes('RGBA', (180, 180), rawImage)
+    print(imageData.size)
     new_image = np.expand_dims(image.img_to_array(imageData), axis=0)
     print(new_image)
     result = loaded_model.predict(new_image)
